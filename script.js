a50623cc410fef22a8f4c5c68f75ab92ef14c230
ella gortsinskaja <ella.gortsinskaja@ivkhk.ee>
2021-09-06 08:47:26 +0300

    objects deconstruction v2.0

--- script.js
+++ script.js
@@ -11,10 +11,19 @@ const game = {
     EBO: "ctx",
     version: "alpha 0.0000000000001.01 steam greenlight edition",
     fillColor: "FF00FF",
-    author: "Buslik"
+    author: "Buslik",
+    method1: function(){
+        console.log("kjgdfkjghn")
+    },
+    method2: function(){
+        console.log("3982749823")
+    }
 };
 
 const {id,width} = game.window;
+
+game.method1()
+game.method2()
 
 console.log(id);
 console.log(width);
